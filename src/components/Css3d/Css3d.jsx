0a755b4153828463ce28540126972d285223e049
alexxxcs1 <534743239@qq.com>
2018-06-22 14:26:10 +0800

    添加css3d鼠标旋转操作

--- src/components/Css3d/Css3d.jsx
+++ src/components/Css3d/Css3d.jsx
@@ -7,7 +7,11 @@ export class Css3d extends Component {
   constructor(prop)
   { 
     super(prop);
-    this.state = {};
+    this.state = {
+      rotateY:0,
+      interval:null,
+      steep:0.1,
+    };
     this.createBkgItem = this.createBkgItem.bind(this);
     this.calTranslateZ = this.calTranslateZ.bind(this);
   }
@@ -18,29 +22,77 @@ export class Css3d extends Component {
   calTranslateZ(opts) {
     return Math.round(opts.width / (2 * Math.tan(Math.PI / opts.number)))
   }
+  setPassword(index)
+  {
+      index==7?console.log('gamestart'):'';  //按到机关，跳转
+  }
+  c_MouseOver(e){
+    if (e.clientX/window.innerWidth<0.4) {
+      this.setState({
+        steep:0.1,
+      })
+    }else if(e.clientX/window.innerWidth>0.6)
+    {
+      this.setState({
+        steep:-0.1,
+      })
+    }else
+    {
+      this.setState({
+        steep:0,
+      })
+    }
+  }
+  c_MouseEnter(e)
+  { 
+    if (!this.state.interval) {
+      var self = this;
+      this.state.interval= setInterval(()=>{
+        self.setState({
+          rotateY:self.state.rotateY+this.state.steep,
+        })
+      },1);
+      this.setState({
+        interval:this.state.interval,
+      })
+    }
+  }
+  c_MouseLeave(e)
+  { 
+    clearInterval(this.state.interval);
+    this.setState({
+      interval:null,
+    })
+  }
   createBkgItem()
   {
     var deg = 360 / 20;
     
     var width = 93;
     var R = parseInt((width / 2) / Math.tan((deg/ 2 )* Math.PI / 180) - 1) // tan@ = 对边(R) / 临边(W/2)
-    console.log(R);
     var width2rem = parseFloat(width) / window.rem;
     var cont = this;
     var startDeg = 180;
+    var self = this;
     var itemNodes = images.map(function(itemBase,index) {   
       startDeg -= deg;
       return (
-        <div className={style.item} style={{width:width2rem+'rem',backgroundImage:'url('+itemBase+')',transform:'translateX(-50%) rotateY('+startDeg+'deg) translateZ('+(-R)+'px)',}}/>
+        <div
+        key={'item'+Math.random()*1024} 
+        className={style.item} onClick={self.setPassword.bind(self,index)} 
+         style={{width:width2rem+'rem',backgroundImage:'url('+itemBase+')',transform:'translateX(-50%) rotateY('+startDeg+'deg) translateZ('+(-R)+'px)',}}/>
       ); 
     });
     return itemNodes;
   }
   render() {
     return (
-        <div className={style.hiddenbox}>
+        <div className={style.hiddenbox} 
+        onMouseOver={this.c_MouseOver.bind(this)}
+        onMouseEnter={this.c_MouseEnter.bind(this)}
+        onMouseLeave={this.c_MouseLeave.bind(this)}>
             <div className={style.outbox}>
-                <div className={style.Box}>
+                <div className={style.Box} style={{transform:'rotateY('+this.state.rotateY+'deg)'}}>
                     {this.createBkgItem()}
                 </div>
             </div>
